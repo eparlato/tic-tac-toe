cccba6f2d7e3d368f2468be835d2f6eebf93eda5
Edoardo Parlato <eparlato82@gmail.com>
2022-04-08 11:22:58 +0200

    add isSatisfiedBy method to RepeatAction BoardGameRule

--- src/main/java/it/eparlato/tictactoe/referee/RepeatAction.java
+++ src/main/java/it/eparlato/tictactoe/referee/RepeatAction.java
@@ -1,8 +1,14 @@
 package it.eparlato.tictactoe.referee;
 
+import it.eparlato.tictactoe.Board;
+import it.eparlato.tictactoe.BoardState;
 import it.eparlato.tictactoe.Game;
 
 public class RepeatAction implements BoardGameRule {
+    public boolean isSatisfiedBy(Board board) {
+        return board.state().equals(BoardState.FIELD_ALREADY_TAKEN);
+    }
+
     @Override
     public void applyOn(Game game) {
         game.repeat();
--- src/test/java/it/eparlato/tictactoe/referee/RepeatActionTest.java
+++ src/test/java/it/eparlato/tictactoe/referee/RepeatActionTest.java
@@ -1,30 +1,43 @@
 package it.eparlato.tictactoe.referee;
 
+import it.eparlato.tictactoe.Board;
+import it.eparlato.tictactoe.BoardState;
 import it.eparlato.tictactoe.Game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class RepeatActionTest {
     private final Game game = mock(Game.class);
-    private BoardGameRule evaluation;
+    private final Board board = mock(Board.class);
+    private final RepeatAction rule = new RepeatAction();
 
-    @BeforeEach
-    void setUp() {
-        evaluation = new RepeatAction();
+    @Test
+    void is_satisfied_by_a_board_where_a_field_has_already_been_taken() {
+        when(board.state()).thenReturn(BoardState.FIELD_ALREADY_TAKEN);
+
+        assertThat(rule.isSatisfiedBy(board)).isTrue();
+    }
+
+    @Test
+    void is_not_satisfied_by_a_board_where_a_field_has_been_taken() {
+        when(board.state()).thenReturn(BoardState.FIELD_TAKEN);
+
+        assertThat(rule.isSatisfiedBy(board)).isFalse();
     }
 
     @Test
     void tells_game_to_repeat() {
-        evaluation.applyOn(game);
+        rule.applyOn(game);
 
         verify(game).repeat();
     }
 
     @Test
     void do_not_tell_game_to_switch_current_player() {
-        evaluation.applyOn(game);
+        rule.applyOn(game);
 
         verify(game, never()).switchPlayer();
     }
